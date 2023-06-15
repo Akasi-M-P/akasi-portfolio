ccdbdab1fca7810f83bc5b60fa064e71e32729bc
Mba96 <mbajameel90@gmail.com>
2023-06-15 00:14:43 +0000

    update

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,4 +1,23 @@
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <main data-aos="fade-up">
+        <h1>About</h1>
+        <p>
+          lorem What are values in JavaScript? JavaScript values() | How
+          JavaScript values() Method Works? JavaScript values are the values
+          that comprise values like Booleans, Strings, arrays, numbers, etc. Its
+          behavior is the same as other programming languages as values are
+          possessing some definite properties which is being used as per the
+          requirement and run time environment while executing the JavaScript.
+        </p>
+      </main>
+    </>
+  );
 };
 export default About;
--- src/Components/TechStack/TechStackItems.jsx
+++ src/Components/TechStack/TechStackItems.jsx
@@ -1,14 +1,17 @@
 import {data} from "./StackData"
 import "/src/css/TechStack.css"
-
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
 
 const TechStackItems = () => {
   return (
     <>
-      <main>
-       <div className="stack-header">
-        <h3>Tech Stack</h3>
-       </div>
+      <main data-aos="fade-up">
+        <div className="stack-header">
+          <h3>Tech Stack</h3>
+        </div>
         <div className="stack-container">
           {data.map((item) => {
             return (
@@ -17,12 +20,12 @@ const TechStackItems = () => {
                   <img src={item.img} alt={item.name} />
                   <p>{item.name}</p>
                 </div>
-              </div>  
+              </div>
             );
           })}
         </div>
       </main>
     </>
   );
-}
+};
 export default TechStackItems
