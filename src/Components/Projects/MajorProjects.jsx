e97d9252f51d449d7d4af774042587163852ee49
Mba96 <mbajameel90@gmail.com>
2023-06-16 18:49:08 +0000

    update

--- src/Components/Projects/MajorProjects.jsx
+++ src/Components/Projects/MajorProjects.jsx
@@ -8,7 +8,11 @@ AOS.init();
 const MajorProjects = () => {
   return (
     <>
-      <main className="project-container">
+      <main
+        className="project-container"
+        data-aos="fade-up"
+        data-aos-duration="3000"
+      >
         <div className="major_projects-header">
           <h3>Major Projects</h3>
         </div>
@@ -22,6 +26,24 @@ const MajorProjects = () => {
                 <div className="project-details">
                   <h4>{project.name}</h4>
                   <p>{project.description}</p>
+                </div>
+                <div className="project-links">
+                  <a
+                    href={project.github}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <img src="/images/contact-links/github.png" alt="github" />
+                  </a>
+                  <button>
+                    <a
+                      href={project.preview}
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      <h5>Preview</h5>
+                    </a>
+                  </button>
                 </div>
               </div>
             );
--- src/Components/Projects/MajorProjectsData.jsx
+++ src/Components/Projects/MajorProjectsData.jsx
@@ -5,6 +5,8 @@ export const data = [
     img: "/images/major-projects/blog-app.png",
     description:
       "Built using the powerful trio of Node.js, Express.js, and MongoDB, provides you with an environment to create, manage, and publish blog posts.",
+    github: "https://github.com/Akasi-M-P/My-Blog-App.git",
+    preview: "https://akasi-blog-app.onrender.com/",
   },
   {
     id: "2",
@@ -12,6 +14,8 @@ export const data = [
     img: "/images/major-projects/shopping-cart.png",
     description:
       "Built using the powerful combination of React.js and Redux.  The cart app allows you to effortlessly add, remove, and update items in your cart. With the integration of Redux, the app centralizes the state of your cart, ensuring that it remains consistent across different components and screens.",
+    github: "https://github.com/Akasi-M-P/shopping-cart-redux.git",
+    preview: "https://akasi-shopping-cart-redux.vercel.app/",
   },
   {
     id: "3",
@@ -19,6 +23,8 @@ export const data = [
     img: "/images/major-projects/to-do-list.png",
     description:
       "Built using Node.js, Express.js, and MongoDB. Designed to enable you create, managing, and organizing your daily tasks.",
+    github: "https://github.com/Akasi-M-P/Node.js-ToDoList-App.git",
+    preview: "https://akasi-todo-app.onrender.com/",
   },
   {
     id: "4",
@@ -26,5 +32,7 @@ export const data = [
     img: "/images/major-projects/contact-card.png",
     description:
       "Built using the powerful combination of React.js and Bootstrap. Provides a modern and intuitive user interface that allows you to effortlessly create, view, update, and delete contact cards",
+    github: "https://github.com/Akasi-M-P/contact-card.git",
+    preview: "https://akasi-react-contact-card.vercel.app/",
   },
 ];
